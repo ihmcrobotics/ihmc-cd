5fd775d2e0d9360523eadbc7405e73097c3ea55e
Duncan Calvert <dcalvert@ihmc.us>
2020-11-27 13:46:01 -0600

    :arrow_up: ihmc-ci 7.4 Gradle 6 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
    kotlin("jvm") version "1.3.41"
    id("us.ihmc.ihmc-build")
-   id("us.ihmc.ihmc-ci") version "7.3"
+   id("us.ihmc.ihmc-ci") version "7.4"
    id("us.ihmc.ihmc-cd")
    id("us.ihmc.log-tools-plugin") version "0.5.0"
 }
